01c8eefd1048dd3d74e83a78668f489e33b83735
Giuseppe Consoli <peppe877@gmail.com>
2021-10-18 11:34:07 +0200

    i3 execs moved to xprofile

--- .scripts/setdisplay.sh
+++ .scripts/setdisplay.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-xrandr --output DP-2 --primary --mode 2560x1440 --rate 120 --scale 1.0x1.0 --output HDMI-0 --left-of DP-2 --auto --scale 1.0x1.0
+xrandr --output DP-0 --primary --mode 2560x1440 --rate 144 --scale 1.0x1.0 --output HDMI-1 --left-of DP-2 --auto --scale 1.0x1.0
 # --primary --mode 2560x1440 --rate 120 --panning 0x0+0+0
