9d89b16dbd966beeadc022061d47095800fd230a
Giuseppe Consoli <peppe877@gmail.com>
2022-10-25 10:29:49 +0200

    git alias, script fixes

--- .scripts/ksm_rewards.sh
+++ .scripts/ksm_rewards.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 # Adds together the numbers in the "Rewards" (5th) column of a CSV generated from subscan.io
-awk -F',' '$NF == "staking(Reward)" { sum += $(NF-1) } END { print sum}' $1
+awk -F',' '$NF == "Staking (Rewarded)" { sum += $(NF-1) } END { print sum}' $1
